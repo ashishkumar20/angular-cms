be5114fa16e051896214d116cc3eec3e71f6cf65
Ashish kumar <ashish.kumar@fci-ccm.com>
2018-09-13 18:17:45 +0530

    ashish commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FullpostComponent } from './fullpost/fullpost.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllPostsComponent,
+    FullpostComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/fullpost/fullpost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fullpost',
+  templateUrl: './fullpost.component.html',
+  styleUrls: ['./fullpost.component.css']
+})
+export class FullpostComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit() {
+  }
+
+}
