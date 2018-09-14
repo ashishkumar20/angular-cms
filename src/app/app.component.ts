794000f895598f3ef9a058cf54e27e7de82c780e
Shalini Tiwari <shalini.tiwari@fci-ccm.com>
2018-09-14 12:13:26 +0530

    Shalini commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map } from 'rxjs/operators';
+import{Http} from '@angular/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cms';
+  constructor(private http: Http) { }
+   httpdata;
+   ngOnInit() {
+      this.http.get("https://jsonplaceholder.typicode.com/posts").
+      pipe(map((response) => response.json())).
+      subscribe(
+         (data) =>{this.displaydata(data);}
+      )
+   }
+   displaydata(data) {this.httpdata = data;}
+   btnClick= function () {
+    this.router.navigate(['./createpost.component.html']);
+  
+};
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material';
+import {HttpModule} from '@angular/http';
+import { CreatepostComponent } from './createpost/createpost.component';
+import{RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatepostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+   HttpModule,
+   RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
